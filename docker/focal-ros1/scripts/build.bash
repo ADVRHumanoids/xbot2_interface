ec76f79f42bb66344801c218b05963baea8141db
Arturo Laurenzi <arturo.laurenzi@gmail.com>
2024-07-31 21:46:18 +0200

    one job in ci

--- docker/focal-ros1/scripts/build.bash
+++ docker/focal-ros1/scripts/build.bash
@@ -24,9 +24,9 @@ forest add-recipes git@github.com:advrhumanoids/multidof_recipes.git -t next
 
 # build
 export PYTHONUNBUFFERED=1
-forest grow xbot2_interface -j6 --verbose --clone-depth 1
+forest grow xbot2_interface --verbose --clone-depth 1
 
 # build tests
 cd build/xbot2_interface
 cmake -DXBOT2_IFC_BUILD_TESTS=1 .
-make -j6
+make
