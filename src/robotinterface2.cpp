80c509b5d470ec5cc486df16b2eb6a48a1119a04
NiccoGazza <niccogazza@hotmail.it>
2024-12-04 16:10:00 +0100

    src/robotinterface2.cpp: robot_type to ros2

--- src/robotinterface2.cpp
+++ src/robotinterface2.cpp
@@ -36,7 +36,7 @@ RobotInterface::UniquePtr RobotInterface::getRobot(ConfigOptions opt)
 {
     auto mdl = ModelInterface::getModel(opt);
 
-    std::string robot_type = "ros";
+    std::string robot_type = "ros2";
 
     opt.get_parameter("robot_type", robot_type);
 
