235ad91f5bbb4ced34a44de9e8422cf9a4423305
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-09-09 15:17:42 +0000

    add normals consistency checks (fails with simple_box.stl)

--- test/src/test_collision.cpp
+++ test/src/test_collision.cpp
@@ -9,12 +9,32 @@ struct TestCollision : TestWithModel
 
     TestCollision()
     {
-        // urdf_path = XBOT2_TEST_RESOURCE_DIR "collision_test_robot.urdf.xml";
-        // srdf_path = "";
     }
 
     virtual void SetUp()
     {
+        urdf_path = XBOT2_TEST_RESOURCE_DIR "centauro_capsule.urdf";
+        srdf_path = XBOT2_TEST_RESOURCE_DIR "centauro_capsule.srdf";
+
+        TestWithModel::SetUp();
+
+        cm = std::make_shared<XBot::Collision::CollisionModel>(model);
+    }
+};
+
+struct TestCollisionNoRobot : TestWithModel
+{
+    std::shared_ptr<XBot::Collision::CollisionModel> cm;
+
+    TestCollisionNoRobot()
+    {
+    }
+
+    virtual void SetUp()
+    {
+        urdf_path = XBOT2_TEST_RESOURCE_DIR "collision_test_robot.urdf.xml";
+        srdf_path = "";
+
         TestWithModel::SetUp();
 
         cm = std::make_shared<XBot::Collision::CollisionModel>(model);
@@ -24,7 +44,6 @@ struct TestCollision : TestWithModel
 struct TestCollisionWithParam : TestCollision,
                                 ::testing::WithParamInterface<bool>
 {
-
 };
 
 TEST_F(TestCollision, checkSize)
@@ -37,7 +56,6 @@ TEST_F(TestCollision, checkSize)
     ASSERT_EQ(cm->getWitnessPoints().size(), cm->getNumCollisionPairs());
     ASSERT_EQ(cm->getNormals().size(), cm->getNumCollisionPairs());
     ASSERT_EQ(cm->getDistanceJacobian().cols(), model->getNv());
-
 }
 
 TEST_F(TestCollision, checkBroadphase)
@@ -51,7 +69,7 @@ TEST_F(TestCollision, checkBroadphase)
     EXPECT_TRUE((cm->computeDistance().array() < 0).any());
     EXPECT_GT(coll_pair_ids.size(), 0);
 
-    for(auto id : coll_pair_ids)
+    for (auto id : coll_pair_ids)
     {
         auto [l1, l2] = cm->getCollisionPairs().at(id);
 
@@ -85,13 +103,13 @@ TEST_P(TestCollisionWithParam, checkFixCollision)
     auto coll_pairs = cm->getCollisionPairs(include_env);
 
     Eigen::MatrixXd Jn(coll_pairs.size(), model->getNv());
-    Eigen::VectorXd en(coll_pairs.size());  // des - actual
+    Eigen::VectorXd en(coll_pairs.size()); // des - actual
 
-    for(int k = 0; true; k++)
+    for (int k = 0; true; k++)
     {
         Eigen::VectorXd d = cm->computeDistance(include_env, d_th);
 
-        if(d.minCoeff() > min_d - err_th)
+        if (d.minCoeff() > min_d - err_th)
         {
             break;
         }
@@ -102,9 +120,9 @@ TEST_P(TestCollisionWithParam, checkFixCollision)
 
         int nn = 0;
 
-        for(int i = 0; i < d.size(); i++)
+        for (int i = 0; i < d.size(); i++)
         {
-            if(d[i] >= min_d)
+            if (d[i] >= min_d)
             {
                 continue;
             }
@@ -119,7 +137,7 @@ TEST_P(TestCollisionWithParam, checkFixCollision)
         auto Jn_block = Jn.topRows(nn);
         auto en_block = en.head(nn);
 
-        auto svd = Jn_block.jacobiSvd(Eigen::ComputeThinU|Eigen::ComputeThinV);
+        auto svd = Jn_block.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV);
         svd.setThreshold(svd.singularValues()[0] / 10.0);
 
         Eigen::VectorXd dq = lam * svd.solve(en_block);
@@ -139,15 +157,12 @@ TEST_P(TestCollisionWithParam, checkFixCollision)
         model->update();
 
         cm->update();
-
     }
 
     std::cout << "final q = " << model->getJointPosition().transpose() << "\n";
 
     EXPECT_FALSE(cm->checkCollision(include_env));
-
 }
-
 
 INSTANTIATE_TEST_SUITE_P(
     checkFixCollision,
@@ -176,7 +191,7 @@ TEST_F(TestCollision, checkEnvironment)
 
     Eigen::Affine3d w_T_c;
     w_T_c.setIdentity();
-    w_T_c.translation() << 3, 3, 3;  // very far away
+    w_T_c.translation() << 3, 3, 3; // very far away
 
     cm->addCollisionShape("mysphere", "world", sp, w_T_c);
 
@@ -205,7 +220,7 @@ TEST_F(TestCollision, checkEnvironment)
     auto wpv = cm->getWitnessPoints(true);
     EXPECT_GT(colliding_idx.size(), 0);
 
-    for(auto i : colliding_idx)
+    for (auto i : colliding_idx)
     {
         // check all colliding pairs are robot-to-env
         EXPECT_EQ(cpairs[i].second, "world");
@@ -213,10 +228,8 @@ TEST_F(TestCollision, checkEnvironment)
         // check env witness point lies on sphere surface
         Eigen::Vector3d wp_env = wpv[i].second;
 
-        EXPECT_FLOAT_EQ((w_T_c.translation() - wp_env).norm(), sp.radius) <<
-            "wp = " << wp_env.transpose() << "\n" <<
-            "sp = " << w_T_c.translation().transpose() << "\n";
-
+        EXPECT_FLOAT_EQ((w_T_c.translation() - wp_env).norm(), sp.radius) << "wp = " << wp_env.transpose() << "\n"
+                                                                          << "sp = " << w_T_c.translation().transpose() << "\n";
     }
 }
 
@@ -302,16 +315,14 @@ TEST_F(TestCollision, checkCollisionVsDistance)
 
     int count = 1000;
 
-    for(int i = 0; i < count; i++)
+    for (int i = 0; i < count; i++)
     {
         check_coll_vs_dist();
     }
 
-
-    std::cout << "CollisionModel::update requires " << dt_upd/count*1e6 << " us \n";
-    std::cout << "CollisionModel::getDistance requires " << dt_dist/count*1e6 << " us \n";
-    std::cout << "CollisionModel::checkSelfCollision requires " << dt_coll/count*1e6 << " us \n";
-
+    std::cout << "CollisionModel::update requires " << dt_upd / count * 1e6 << " us \n";
+    std::cout << "CollisionModel::getDistance requires " << dt_dist / count * 1e6 << " us \n";
+    std::cout << "CollisionModel::checkSelfCollision requires " << dt_coll / count * 1e6 << " us \n";
 }
 
 TEST_F(TestCollision, checkUserCollisionActivation)
@@ -340,7 +351,6 @@ TEST_F(TestCollision, checkUserCollisionActivation)
     cm->setCollisionShapeActive("mysphere", true);
     ASSERT_TRUE(cm->checkCollision());
     ASSERT_TRUE(cm->computeDistance(true).minCoeff() <= 0);
-
 }
 
 TEST_F(TestCollision, checkUserCollisionTouchLinks)
@@ -369,7 +379,7 @@ TEST_F(TestCollision, checkUserCollisionTouchLinks)
     auto d = cm->computeDistance();
     auto pairs = cm->getCollisionPairs();
 
-    for(int i : coll_idx)
+    for (int i : coll_idx)
     {
         fmt::print("{} vs {} -> d = {} \n",
                    pairs[i].first, pairs[i].second, d[i]);
@@ -386,10 +396,6 @@ TEST_F(TestCollision, checkUserCollisionTouchLinks)
     EXPECT_GT(n2, n1);
     EXPECT_TRUE(cm->checkCollision(coll_idx));
     EXPECT_TRUE(cm->computeDistance().minCoeff() <= 0);
-
-
-
-
 }
 
 TEST_F(TestCollision, checkWpNormals)
@@ -397,7 +403,7 @@ TEST_F(TestCollision, checkWpNormals)
 
     auto check_wp_normals = [&]()
     {
-        auto qrand = model->sum(model->getNeutralQ(), 3*Eigen::VectorXd::Random(model->getNv()));
+        auto qrand = model->sum(model->getNeutralQ(), 3 * Eigen::VectorXd::Random(model->getNv()));
         model->setJointPosition(qrand);
         model->update();
 
@@ -408,41 +414,37 @@ TEST_F(TestCollision, checkWpNormals)
         auto n = cm->getNormals();
         auto lp = cm->getCollisionPairs();
 
-        for(int i = 0; i < cm->getNumCollisionPairs(); i++)
+        for (int i = 0; i < cm->getNumCollisionPairs(); i++)
         {
             auto [l1, l2] = lp[i];
             auto [w1, w2] = wp[i];
 
-            EXPECT_NEAR((w1 - w2).norm(), fabs(d[i]), 1e-3) <<
-                l1 << " vs " << l2 << ": d = " << d[i] << "\n" <<
-                "d0 = " << (w1 - w2).norm() << "\n" <<
-                "d1 = " << fabs(d[i]) << "\n";
+            EXPECT_NEAR((w1 - w2).norm(), fabs(d[i]), 1e-3) << l1 << " vs " << l2 << ": d = " << d[i] << "\n"
+                                                            << "d0 = " << (w1 - w2).norm() << "\n"
+                                                            << "d1 = " << fabs(d[i]) << "\n";
 
             Eigen::Vector3d n_from_wp = (w2 - w1).normalized() * (d[i] < 0 ? -1 : 1);
 
-            EXPECT_LT((n[i] - n_from_wp).lpNorm<Eigen::Infinity>(), 1e-3) <<
-                l1 << " vs " << l2 << ": d = " << d[i] << "\n" <<
-                "n0 = " << n[i].transpose() << "\n" <<
-                "n1 = " << n_from_wp.transpose() << "\n";
+            EXPECT_LT((n[i] - n_from_wp).lpNorm<Eigen::Infinity>(), 1e-3) << l1 << " vs " << l2 << ": d = " << d[i] << "\n"
+                                                                          << "n0 = " << n[i].transpose() << "\n"
+                                                                          << "n1 = " << n_from_wp.transpose() << "\n";
         }
-
     };
 
     int count = 1001;
 
-    for(int i = 0; i < count; i++)
+    for (int i = 0; i < count; i++)
     {
         check_wp_normals();
     }
 }
-
 
 TEST_F(TestCollision, checkOrderedIndices)
 {
 
     auto check_ordered_idx = [&]()
     {
-        auto qrand = model->sum(model->getNeutralQ(), 3*Eigen::VectorXd::Random(model->getNv()));
+        auto qrand = model->sum(model->getNeutralQ(), 3 * Eigen::VectorXd::Random(model->getNv()));
         model->setJointPosition(qrand);
         model->update();
 
@@ -454,23 +456,19 @@ TEST_F(TestCollision, checkOrderedIndices)
 
         ASSERT_EQ(ordered_idx.size(), cm->getNumCollisionPairs());
 
-        for(int i = 0; i < cm->getNumCollisionPairs() - 1; i++)
+        for (int i = 0; i < cm->getNumCollisionPairs() - 1; i++)
         {
-            EXPECT_LE(d(ordered_idx[i]), d(ordered_idx[i+1]));
+            EXPECT_LE(d(ordered_idx[i]), d(ordered_idx[i + 1]));
         }
-
     };
 
     int count = 1001;
 
-    for(int i = 0; i < count; i++)
+    for (int i = 0; i < count; i++)
     {
         check_ordered_idx();
     }
-
-
 }
-
 
 TEST_F(TestCollision, checkJacobian)
 {
@@ -481,7 +479,7 @@ TEST_F(TestCollision, checkJacobian)
 
     auto check_jac = [&]()
     {
-        auto qrand = model->sum(model->getNeutralQ(), 3*Eigen::VectorXd::Random(model->getNv()));
+        auto qrand = model->sum(model->getNeutralQ(), 3 * Eigen::VectorXd::Random(model->getNv()));
         model->setJointPosition(qrand);
         model->update();
 
@@ -500,12 +498,11 @@ TEST_F(TestCollision, checkJacobian)
         Eigen::MatrixXd Jhat;
         Jhat.resizeLike(J);
 
-
         double h = 1e-4;
 
-        for(int i = 0; i < model->getNv(); i++)
+        for (int i = 0; i < model->getNv(); i++)
         {
-            Eigen::VectorXd dq = Eigen::VectorXd::Unit(model->getNv(), i)*h;
+            Eigen::VectorXd dq = Eigen::VectorXd::Unit(model->getNv(), i) * h;
 
             model->setJointPosition(model->sum(qrand, dq));
             // std::cout << i << " qplus  " << model->getJointPosition().transpose().format(5) << "\n";
@@ -519,25 +516,23 @@ TEST_F(TestCollision, checkJacobian)
             cm->update();
             auto dminus = cm->computeDistance(th);
 
-
             // std::cout << i << " dplus  " << dplus.transpose().format(5) << "\n";
             // std::cout << i << " d0     " << d0.transpose().format(5) << "\n";
             // std::cout << i << " dminus " << dminus.transpose().format(5) << "\n\n";
 
-            Jhat.col(i) = (dplus - dminus) / (2*h);
+            Jhat.col(i) = (dplus - dminus) / (2 * h);
         }
 
         double Jerr = (J - Jhat).lpNorm<Eigen::Infinity>();
 
-
         EXPECT_LT(Jerr, 0.5);
 
         int i = 0;
-        for(auto [l1, l2] : cm->getCollisionPairs())
+        for (auto [l1, l2] : cm->getCollisionPairs())
         {
             double err = (J.row(i) - Jhat.row(i)).lpNorm<Eigen::Infinity>();
 
-            if(err > 1e-1)
+            if (err > 1e-1)
             {
                 std::cout << l1 << " vs " << l2 << ": err = " << err << ", dist = " << d0[i] << "\n";
                 std::cout << "Jhat = " << Jhat.row(i).format(4) << "\n";
@@ -546,24 +541,123 @@ TEST_F(TestCollision, checkJacobian)
 
             i++;
         }
-
     };
 
     int count = 1001;
 
-    for(int i = 0; i < count; i++)
+    for (int i = 0; i < count; i++)
     {
         check_jac();
     }
 
-    std::cout << "CollisionModel::update requires " << dt_upd/count*1e6 << " us \n";
-    std::cout << "CollisionModel::getDistance requires " << dt_dist/count*1e6 << " us \n";
-    std::cout << "CollisionModel::getDistanceJacobian requires " << dt_jac/count*1e6 << " us \n";
-
+    std::cout << "CollisionModel::update requires " << dt_upd / count * 1e6 << " us \n";
+    std::cout << "CollisionModel::getDistance requires " << dt_dist / count * 1e6 << " us \n";
+    std::cout << "CollisionModel::getDistanceJacobian requires " << dt_jac / count * 1e6 << " us \n";
 }
 
+TEST_F(TestCollisionNoRobot, checkDistanceConsistency)
+{
+    const bool use_mesh = false;
 
-int main(int argc, char ** argv)
+    Eigen::Affine3d w_T_c;
+    w_T_c.setIdentity();
+    w_T_c.translation() << 2.0, 0, 0; // very far away
+
+    if(use_mesh)
+    {
+        // add a box in the environment
+        XBot::Collision::Shape::Mesh mesh;
+        mesh.filepath = "file://" XBOT2_TEST_RESOURCE_DIR "simple_box.stl";
+        mesh.scale = Eigen::Vector3d::Ones();
+        cm->addCollisionShape("mybox", "world", mesh, w_T_c);
+    }
+    else
+    {
+        // add a box in the environment
+        XBot::Collision::Shape::Box box;
+        box.size = Eigen::Vector3d::Constant(1.0);
+        cm->addCollisionShape("mybox", "world", box, w_T_c);
+    }
+    
+    
+    
+    const bool include_env = true;
+    
+    model->setJointPosition(model->getNeutralQ());
+    model->update();
+    cm->update();
+
+    auto d = cm->computeDistance(include_env);
+    auto wp = cm->getWitnessPoints(include_env);
+    auto n = cm->getNormals(include_env);
+    auto lp = cm->getCollisionPairs(include_env);
+
+    for (int i = 0; i < cm->getNumCollisionPairs(include_env); i++)
+    {
+        auto [l1, l2] = lp[i];
+        auto [w1, w2] = wp[i];
+
+        std::cout << l1 << " vs " << l2 << ": d = " << d[i] << "\n"
+                  << "w1 = " << w1.transpose() << "\n"
+                  << "w2 = " << w2.transpose() << "\n"
+                  << "n  = " << n[i].transpose() << "\n\n";
+
+        // check distance vs wp consistency
+        EXPECT_NEAR((w1 - w2).norm(), fabs(d[i]), 1e-3) << l1 << " vs " << l2 << ": d = " << d[i] << "\n"
+                                                        << "d0 = " << (w1 - w2).norm() << "\n"
+                                                        << "d1 = " << fabs(d[i]) << "\n";
+        
+        // check normal vs wp consistency
+        Eigen::Vector3d n_from_wp = (w2 - w1).normalized() * (d[i] < 0 ? -1 : 1);
+
+        EXPECT_LT((n[i] - n_from_wp).lpNorm<Eigen::Infinity>(), 1e-3) << l1 << " vs " << l2 << ": d = " << d[i] << "\n"
+                                                                      << "n0 = " << n[i].transpose() << "\n"
+                                                                      << "n1 = " << n_from_wp.transpose() << "\n";
+    }
+
+    Eigen::VectorXd q = model->getJointPosition();
+    const double step = 0.01;
+
+    auto decrease_distance = [&]()
+    {
+        auto J = cm->getDistanceJacobian(include_env);
+        Eigen::VectorXd dq = -J.transpose() * Eigen::VectorXd::Ones(J.rows()) * step;
+        q = model->sum(q, dq);
+
+        model->setJointPosition(q);
+        model->update();
+        cm->update();
+
+        auto d_new = cm->computeDistance(include_env);
+        auto n_new = cm->getNormals(include_env);
+
+        for (int i = 0; i < d.size(); i++)
+        {
+            // expect distance to decrease
+            EXPECT_LE(d_new[i], d[i]) << "pair " << lp[i].first << " vs " << lp[i].second << "\n"
+                                      << "old d: " << d[i] << "\n"
+                                      << "new d: " << d_new[i] << "\n";
+
+            // expect normals not to flip
+            EXPECT_GE(n_new[i].dot(n[i]), -0.1) << "pair " << lp[i].first << " vs " << lp[i].second << "\n"
+                                            << "old n: " << n[i].transpose() << "\n"
+                                            << "new n: " << n_new[i].transpose() << "\n";
+
+            // update distance and normal
+            n[i] = n_new[i];
+            d[i] = d_new[i];
+        }
+    };
+
+    for(int i = 0; i < 25; i++)
+    {
+        std::cout << "Step " << i << "\n";
+        std::cout << "d = " << d.tail(8).transpose().format(2) << "\n";
+        decrease_distance();
+    }
+}
+
+int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
 
